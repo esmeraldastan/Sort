3acde4facd4a27b25f89ca31441ce6e6ed1368de
Chase <esmeraldarekz99@gmail.com>
2017-01-19 12:59:28 -0800

    sort

---
+++ Sort/MySort.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class MySort here.
+ * 
+ * @author (your name) 
+ * @version 1.19.17
+ */
+// at least 12 different numbers
+//doubles
+import java.util.ArrayList;
+import java.util.List;
+public class MySort{
+    ArrayList<Double> sorting;
+    pubic MySort(){
+        sorting = new ArrayList<Double>();
+        sorting.add(23);
+        sorting.add(15);
+        sorting.add(35);
+    }
+    //print preview
+    public void printPreview(){
+        System.out.prinln(sorting);
+        
+    }
+    
+}
+    
