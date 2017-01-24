c11cdb144707712f043c73ad6f5cc1674fa76af6
Chase <esmeraldarekz99@gmail.com>
2017-01-24 12:57:56 -0800

    1/24 save

--- Sort/MySort.java
+++ Sort/MySort.java
@@ -11,12 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.lang.Math;
 public class MySort{
-    ArrayList<Double> sorting;
-    //public MySort(){
-        // = new ArrayList<Double>();
-       // sorting.add();
-        //sorting.add();
-    
+    ArrayList<Double> sorting = new ArrayList<Double>();
     public MySort(){
         sorting.addAll(RandomArray(10000));// this adds 10,000 random
     }
@@ -25,7 +20,12 @@ public class MySort{
         /**
          * This method creates n random numbers between 0 (including) and 10,000
          */
-    }
+       for (int i=0; i<n; i++)
+        {
+            arrayRandom.add(Math.random()*100);
+        }
+        return arrayRandom;
+     }
     //print preview
     public void printPreview(){
         for( double i = 0; i < sorting.size(); i++){
