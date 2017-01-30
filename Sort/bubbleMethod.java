7e45c0bd16723f0fdbd6d5925a93a01d6896bc12
Chase <esmeraldarekz99@gmail.com>
2017-01-30 12:58:29 -0800

    BubbleMethod

--- Sort/bubbleMethod.java
+++ Sort/bubbleMethod.java
@@ -1,21 +1,20 @@
 
 /**
- * Write a description of class MySort here.
+ * Write a description of class BubbleMethod here.
  * 
  * @author (your name) 
- * @version 1.19.17
+ * @version (a version number or a date)
  */
-// at least 12 different numbers
-//doubles
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.Math;
-public class MySort{
+
+public class bubbleMethod{
+    //random number picker 
     ArrayList<Integer> sorting = new ArrayList<Integer>();
-    public MySort(){
+    public bubbleMethod(){
         sorting.addAll(RandomArray(10000));// this adds 10,000 random
     }
-    
     private static ArrayList<Integer>RandomArray(int n){
         /**
          * This method creates n random numbers between 0 (including) and 10,000
@@ -27,38 +26,27 @@ public class MySort{
         }
         return arrayRandom;
      }
-    //print preview
-    public void printPreview(){
-        for( double i = 0; i < sorting.size(); i++){
-        System.out.println(sorting);
+     
+     public void printPreview(){
+        for( int i = 0; i < sorting.size(); i++){
+        System.out.println(sorting.get(i));
       }
     }
-    
-    //Bubble Method 
-    public static void bubbleMethod(int[] numberlist){
-        int nuM = numberlist.length; 
+     
+     public static void bubbleMethod(int[] arrayRandom){
+        int nuM = arrayRandom.length; 
         int value = 0 ;
         for( int i = 0; i < nuM; i++){
                                      //numberlist - i2
             for( int i2 = i ; i2 < (nuM- i2); i2++){
-                if (numberlist[ i2 - 1 ]> numberlist[i2]){
-                 value = numberlist[i2 - 1];//same as if 
-                 numberlist[i2 - 1] = numberlist[i2];
-                 numberlist[i2] = value;
-                 
-                 
-                 
-            }
-        
-        
-          }
+                if (arrayRandom[ i2 - 1 ]> arrayRandom[i2]){
+                 value = arrayRandom[i2 - 1];//same as if 
+                 arrayRandom[i2 - 1]  = arrayRandom[i2];
+                 arrayRandom[i2] = value;
+           }
+         }
        }
-   
     }
 }
     
-        
-    
-    
-
     
